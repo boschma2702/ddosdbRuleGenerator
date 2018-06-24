9a0e96bcceed00e4d60226ff1e4bd52e31231029
boschma2702 <paardrijdenrep11>
2018-06-24 13:21:42 +0200

    added simple client server example

--- generator.py
+++ generator.py
@@ -5,15 +5,12 @@ add_dst_ports = True
 add_src_ports = True
 pretty_print = True
 
-with open('json_files/7bf0c3f9fff85d3fe81f71279d6ae73e.json') as data_file:
-    d = json.load(data_file)
-
 
 def write_rule(rule_name, data):
-    rule = "signature {} {".format(rule_name)
+    rule = ["signature {} {{".format(rule_name)]
     protocol = data["protocol"]
 
-    if pretty_print: rule += "\n"
+    if pretty_print: rule.append("\n")
 
     if add_src_ip:
         add_to_str(rule, write_src_ips(data["src_ips"]))
@@ -25,6 +22,7 @@ def write_rule(rule_name, data):
         add_to_str(rule, write_dst_ports(data["dst_ports"]))
 
     if protocol == "ICMP":
+        add_to_str(rule, write_icmp_type(int(float(data["additional"]["icmp_type"].split(",")[0]))))
         pass
     elif protocol == "NTP":
         pass
@@ -32,8 +30,13 @@ def write_rule(rule_name, data):
         pass
     elif protocol == "TCP":
         pass
+    elif protocol == "DNS":
+        add_to_str(rule, write_dns_query(data["additional"]["dns_query"]))
+        pass
 
-    rule += "}"
+    rule.append("}")
+
+    return "".join(rule)
 
 
 def write_protocol(protocol: str):
@@ -54,13 +57,19 @@ def write_dst_ips(ips: list):
 
 def write_src_ports(ports: list):
     if len(ports) > 0:
-        return "src-ip == {}".format(", ".join(ports))
+        s = str(int(ports[0]))
+        for i in range(1, len(ports)-1):
+            s += str(int(ports[i]))
+        return "src-ports == {}".format(s)
     return ""
 
 
 def write_dst_ports(ports: list):
     if len(ports) > 0:
-        return "dst-ip == {}".format(", ".join(ports))
+        s = str(int(ports[0]))
+        for i in range(1, len(ports)-1):
+            s += ", "+str(int(ports[i]))
+        return "dst-ports == {}".format(s)
     return ""
 
 
@@ -72,8 +81,14 @@ def write_icmp_type(t: int):
     return "header icmp[0:1] == {}".format(str(t))
 
 
-def add_to_str(rule: str, to_add:str):
-    if pretty_print:
-        rule += "\t"+to_add+"\n"
-    else:
-        rule += to_add
+def add_to_str(rule: list, to_add:str):
+    if not to_add == "":
+        if pretty_print:
+            rule.append("\t"+to_add+"\n")
+        else:
+            rule.append(to_add)
+
+
+with open('json_files/072a16f4c24332ec1dbbc8ea6df36087.json') as data_file:
+    d = json.load(data_file)
+    print(write_rule("test", d))
